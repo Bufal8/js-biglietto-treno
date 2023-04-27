7c82688137ed54be3c00bbf4f0cea6ba30ea22c7
ManuelF <manuel.floriani1999@gmail.com>
2023-04-27 17:03:27 +0200

    dichiaro arrotondamento prima di usarlo

--- main.js
+++ main.js
@@ -16,23 +16,27 @@ userEta = prompt("Quanti anni hai?");
 
 // moltiplicare i chilometri da percorrere per la costante 0.21€
 userTotaleParziale = userChilometri * euroPerChilometro;
+
 // calcolare lo sconto
+let arrotondamento = userTotale.toFixed(2);
+
 // se età < 18 anni sconto 20%
 if(userEta < 18){
     userTotale = userTotaleParziale - (userTotaleParziale * 20 / 100);
     messaggio = `Giovanotto il prezzo del tuo biglietto è ${arrotondamento}€`;
 }
+
 // se età > 65 anni sconto 40%
 else if(userEta >= 65){
     userTotale = userTotaleParziale - (userTotaleParziale * 40 / 100);
     messaggio = `Nonno il prezzo del tuo biglietto è ${arrotondamento}€`;
 }
 
+// età compresa tra 18 e 64 senza sconto
 else{
     userTotale = userTotaleParziale;
     messaggio = `Il prezzo del tuo biglietto è ${arrotondamento}€`;
 }
 
-let arrotondamento = userTotale.toFixed(2);
 
 document.getElementById("mio_id").innerHTML = messaggio;
